6b502599c279e20fe97c10f0b85eddf4e142777a
Vinicius N. Lima <53085763+vinicius190@users.noreply.github.com>
2023-06-01 20:39:18 -0300

    upload app

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -3,12 +3,14 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://0.0.0.0/esoft-nest-7s'),
     UsersModule,
     BooksModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
